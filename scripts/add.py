8ef28db585e1eb34bf051d1e4c5972f267cfe162
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2024-05-06 00:38:13 +0300

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -4,6 +4,8 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 # User can add name and favourite sport in response.json
+name='Firat'
+sport='chess'
 # default sport Cricket will be added incase user does not provide fav sport
 
 
